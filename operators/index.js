b81303705a2b48fa052258121da4d3687a3c4745
deliwalayash <deliwalayash2@gmail.com>
2025-06-19 15:30:32 +0530

    operator done

---
+++ operators/index.js
@@ -0,0 +1,20 @@
+//arithmetic operator +,-,*,/,%
+//unary operator ++,--,** power
+
+let a = 2
+let b = 5
+let c=50
+let d ="5"
+let e=5.65
+let f="5.6589"
+console.log(a + b)
+console.log(a ** b) // power
+console.log(a++)    // first print than increase
+console.log(++a)    //first increase than print
+console.log(c.toString(2)) // convert to binary
+console.log(a+d)
+console.log(a + parseInt(e))  //convert to int
+console.log(a+e)
+console.log(a+f)
+console.log(a+parseFloat(f))  //covert to float
+
